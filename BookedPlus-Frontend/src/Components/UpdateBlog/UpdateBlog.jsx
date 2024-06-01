c073deb033937dbaf16967adf19ea71cbc445b47
Akil Tajwar <akiltajwar@gmail.com>
2024-06-02 04:44:35 +0600

    blog data is ready to send in backend

--- BookedPlus-Frontend/src/Components/UpdateBlog/UpdateBlog.jsx
+++ BookedPlus-Frontend/src/Components/UpdateBlog/UpdateBlog.jsx
@@ -4,17 +4,19 @@ import JoditEditor from "jodit-react";
 const UpdateBlog = () => {
     const editor1 = useRef(null);
     return (
-        <div className="bg-gray-200 p-10 m-10 rounded-md text-black">
-            <div className="">
+        <div className="bg-gray-200 p-10 m-10  text-black">
+            <div className="rounded-none">
                 <h2 id="formTitle" className='text-3xl font-semibold text-center pb-5'>Update Blog</h2>
                 <form id="authorForm">
                     <div className="form-group">
                         <label for="firstName">Blog Title</label>
                         <input type="text" id="blogtitle" name="firstName" required />
                     </div>
-                    <div className="form-group">
-                    <label for="firstName">Blog Body</label>
-                        <JoditEditor name='blogBody' ref={editor1} required />
+                    <div className="form-group text-black rounded-none no-tailwind">
+                        <label for="firstName">Blog Body</label>
+                        <div className="no-tailwind text-black">
+                            <JoditEditor className="rounded-none" name='blogBody' ref={editor1} required />
+                        </div>
                     </div>
                     <div className="form-actions">
                         <button type="submit" className="btn-save">Save</button>
