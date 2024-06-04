5b2d27ba8d14e6163430f6ec9f922088bd5e0912
ajfahim <ajfahim52@gmail.com>
2024-06-04 17:42:02 +0600

    api added

--- BookedPlus-Frontend/src/Components/AddBlog/AddBlog.jsx
+++ BookedPlus-Frontend/src/Components/AddBlog/AddBlog.jsx
@@ -1,228 +1,257 @@
-import React, { useEffect, useRef, useState } from "react";
-import JoditEditor from "jodit-react";
-import { FiPlusCircle } from "react-icons/fi";
-import './custom.css';
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../../hooks/useAuthContext';
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import JoditEditor from "jodit-react";
+import React, { useEffect, useRef, useState } from "react";
+import { FiPlusCircle } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import "./custom.css";
 
 const img_hosting_token = import.meta.env.VITE_Image_Upload_token;
 const AddBlog = () => {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
 
-    const {user}=useAuthContext();
+  const { user } = useAuthContext();
 
-    if(!user){
-      navigate('/')
+  if (!user) {
+    navigate("/");
+  }
+  const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
+  const editor1 = useRef(null);
+  const [authors, setAuthors] = useState([]);
+  const [sections, setSections] = useState([]);
+  const [hideIndex, setHideIndex] = useState(-1); // -1 means no button is hidden initially
+
+  // Array to hold refs for each editor in the sections
+  const editorRefs = useRef([]);
+
+  useEffect(() => {
+    fetch("http://194.238.17.44/api/author/")
+      .then((res) => res.json())
+      .then((data) => {
+        setAuthors(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const addSection = () => {
+    setSections([...sections, { id: sections.length }]);
+    setHideIndex(sections.length); // Hide the current button
+
+    // Create a new ref and add it to the array
+    editorRefs.current.push(React.createRef());
+  };
+
+  function getAuthor(author) {
+    for (let i = 0; i < authors.length; i++) {
+      if (authors[i].firstname + " " + authors[i].lastname === author) {
+        return authors[i];
+      }
     }
-    const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
-    const editor1 = useRef(null);
-    const [authors, setAuthors] = useState([]);
-    const [sections, setSections] = useState([]);
-    const [hideIndex, setHideIndex] = useState(-1); // -1 means no button is hidden initially
+  }
 
-    // Array to hold refs for each editor in the sections
-    const editorRefs = useRef([]);
+  const uploadAudio = async (audioFile) => {
+    const formData = new FormData();
+    formData.append("audio", audioFile);
 
-    useEffect(() => {
-        fetch('http://194.238.17.44/api/author/')
-            .then(res => res.json())
-            .then(data => {
-                setAuthors(data);
-                console.log(data);
-            })
-            .catch(error => console.log(error))
-    }, []);
-
-    const addSection = () => {
-        setSections([...sections, { id: sections.length }]);
-        setHideIndex(sections.length); // Hide the current button
-
-        // Create a new ref and add it to the array
-        editorRefs.current.push(React.createRef());
-    };
-
-   
-
-    function getAuthor(author){
-        for(let i=0;i<authors.length;i++){
-            if(authors[i].firstname+' '+authors[i].lastname===author){
-                return authors[i];
-            }
+    try {
+      const response = await axios.post(
+        "http://194.238.17.44/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
+      );
+      return response.data.path; // Assuming the server returns the path of the uploaded file
+    } catch (error) {
+      console.error("Error uploading audio file:", error);
+      throw error;
     }
+  };
+  const uploadImage = async (image) => {
+    const formData = new FormData();
+    formData.append("image", image);
 
-    const uploadAudio = async (audioFile) => {
-        const formData = new FormData();
-        formData.append('audio', audioFile);
-    
-        try {
-            const response = await axios.post('http://194.238.17.44/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            return response.data.path; // Assuming the server returns the path of the uploaded file
-        } catch (error) {
-            console.error('Error uploading audio file:', error);
-            throw error;
+    try {
+      const response = await axios.post(
+        "http://194.238.17.44/api/uploadImage",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
-    };
-    const uploadImage = async (image) => {
-        const formData = new FormData();
-        formData.append('image', image);
-    
-        try {
-            const response = await axios.post('http://194.238.17.44/uploadImage', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            return response.data.path; // Assuming the server returns the path of the uploaded file
-        } catch (error) {
-            console.error('Error uploading image file:', error);
-            throw error;
-        }
-    };
+      );
+      return response.data.path; // Assuming the server returns the path of the uploaded file
+    } catch (error) {
+      console.error("Error uploading image file:", error);
+      throw error;
+    }
+  };
 
-    const createBlog = async (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const thumbFile = form.thumb.files[0];
-        const thumbhead = form.thumbhead.value;
-        const thumbdesc = editor1.current.value;
-        const headline = form.headline.value;
-        const thumbaudio = form.audio.files[0];
-        const author = form.author.value;
+  const createBlog = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const thumbFile = form.thumb.files[0];
+    const thumbhead = form.thumbhead.value;
+    const thumbdesc = editor1.current.value;
+    const headline = form.headline.value;
+    const thumbaudio = form.audio.files[0];
+    const author = form.author.value;
 
-        const thumbImageUrl = thumbFile ? await uploadImage(thumbFile) : null;
-        const thumbaudioUrl = thumbaudio ? await uploadAudio(thumbaudio) : "null";
+    const thumbImageUrl = thumbFile ? await uploadImage(thumbFile) : null;
+    const thumbaudioUrl = thumbaudio ? await uploadAudio(thumbaudio) : "null";
 
-        // Collecting section data with image upload
-        const body = await Promise.all(sections.map(async (section, index) => {
-            const imageFile = form[`image-${section.id}`].files[0];
-            const imageUrl = imageFile ? await uploadImage(imageFile) : null;
-            const sectionHeader = form[`sectionHeader-${section.id}`].value;
-            const desc = editorRefs.current[index].current.value;
+    // Collecting section data with image upload
+    const body = await Promise.all(
+      sections.map(async (section, index) => {
+        const imageFile = form[`image-${section.id}`].files[0];
+        const imageUrl = imageFile ? await uploadImage(imageFile) : null;
+        const sectionHeader = form[`sectionHeader-${section.id}`].value;
+        const desc = editorRefs.current[index].current.value;
 
-            return {
-                image: imageUrl,
-                header:sectionHeader,
-                desc
-            };
-        }));
-
-        const newBlog = {
-            thumbnail: thumbImageUrl,
-            thumbnailheadline:thumbhead,
-            thumbnaildesc:thumbdesc,
-            headline,
-            audio: thumbaudioUrl,
-            author:getAuthor(author),
-            body
+        return {
+          image: imageUrl,
+          header: sectionHeader,
+          desc,
         };
+      })
+    );
 
-        console.log(newBlog);
-        console.log("AADUUUD",newBlog.audio.name)
-
-        // Make your API call here to save the newBlog data
-        try {
-            const response = await axios.post(
-              "http://194.238.17.44/api/blog/",
-              newBlog
-            );
-      
-            console.log("Form submitted successfully!", response.data);
-            e.target.reset();
-            navigate('/665bc136ca6d454ec0f5eed5');
-          } catch (error) {
-            if (error.response.status === 409) {
-              alert("Данные уже существуют");
-            } else {
-              console.log(error);
-            }
-          }
+    const newBlog = {
+      thumbnail: thumbImageUrl,
+      thumbnailheadline: thumbhead,
+      thumbnaildesc: thumbdesc,
+      headline,
+      audio: thumbaudioUrl,
+      author: getAuthor(author),
+      body,
     };
 
-    return (
-        <div className="">
-            <h2 id="formTitle" className='text-3xl font-semibold text-center pb-5'>Add Blog</h2>
-            <form onSubmit={createBlog} id="authorForm">
-                <div className="bg-gray-200 p-10 m-10 rounded-md text-black fixing">
-                    <div className="form-group">
-                        <label>Thumbnail</label>
-                        <input type="file" name="thumb" className="file-input file-input-bordered w-full max-w-xs" />
-                    </div>
-                    <div className="form-group">
-                        <label>Thumbnail Head</label>
-                        <input type="text" name="thumbhead" required />
-                    </div>
-                    <div className="form-group">
-                        <label>Thumbnail Description</label>
-                        <JoditEditor name='thumbdesc' ref={editor1} required />
-                    </div>
-                    <div className="form-group">
-                        <label>Headline</label>
-                        <input type="text" name="headline" required />
-                    </div>
-                    <div className="form-group">
-                        <label>Audio</label>
-                        <input type="file" name="audio" accept="audio/*" />
-                    </div>
-                    <div className="form-group">
-                        <label>Author</label>
-                        <select name="author">
-                            {
-                                authors.map(singleAuthor => (
-                                    <option key={singleAuthor._id}>
-                                        {singleAuthor.firstname + ' ' + singleAuthor.lastname}
-                                    </option>
-                                ))
-                            }
-                        </select>
-                    </div>
-                </div>
-                <div className="flex justify-center">
-                    {hideIndex === -1 && (
-                        <button type="button" onClick={addSection} className="flex items-center gap-1 text-xl bg-[#4299e1] text-white px-3 py-2 rounded">
-                            <FiPlusCircle /> Add Section
-                        </button>
-                    )}
-                </div>
-                <div id="destination-div">
-                    {sections.map((section, index) => (
-                        <div key={section.id}>
-                            <div className="bg-gray-200 p-10 m-10 rounded-md text-black fixing">
-                                <div className="form-group">
-                                    <label>Image</label>
-                                    <input type="file" name={`image-${section.id}`} className="file-input file-input-bordered w-full max-w-xs" />
-                                </div>
-                                <div className="form-group">
-                                    <label>Header</label>
-                                    <input type="text" name={`sectionHeader-${section.id}`}  />
-                                </div>
-                                <div className="form-group">
-                                    <label>Description</label>
-                                    <JoditEditor name={`desc-${section.id}`} ref={editorRefs.current[index]} required />
-                                </div>
-                            </div>
-                            <div className="flex justify-center my-3">
-                                {hideIndex === index && (
-                                    <button type="button" onClick={addSection} className="flex items-center gap-1 text-xl bg-[#4299e1] text-white px-3 py-2 rounded">
-                                        <FiPlusCircle /> Add Section
-                                    </button>
-                                )}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div className="form-actions mx-10">
-                    <button type="submit" className="btn-save">Submit Blog</button>
-                </div>
-            </form>
+    console.log(newBlog);
+    console.log("AADUUUD", newBlog.audio.name);
+
+    // Make your API call here to save the newBlog data
+    try {
+      const response = await axios.post(
+        "http://194.238.17.44/api/blog/",
+        newBlog
+      );
+
+      console.log("Form submitted successfully!", response.data);
+      e.target.reset();
+      navigate("/665bc136ca6d454ec0f5eed5");
+    } catch (error) {
+      if (error.response.status === 409) {
+        alert("Данные уже существуют");
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div className="">
+      <h2 id="formTitle" className="text-3xl font-semibold text-center pb-5">
+        Add Blog
+      </h2>
+      <form onSubmit={createBlog} id="authorForm">
+        <div className="bg-gray-200 p-10 m-10 rounded-md text-black fixing">
+          <div className="form-group">
+            <label>Thumbnail</label>
+            <input
+              type="file"
+              name="thumb"
+              className="file-input file-input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-group">
+            <label>Thumbnail Head</label>
+            <input type="text" name="thumbhead" required />
+          </div>
+          <div className="form-group">
+            <label>Thumbnail Description</label>
+            <JoditEditor name="thumbdesc" ref={editor1} required />
+          </div>
+          <div className="form-group">
+            <label>Headline</label>
+            <input type="text" name="headline" required />
+          </div>
+          <div className="form-group">
+            <label>Audio</label>
+            <input type="file" name="audio" accept="audio/*" />
+          </div>
+          <div className="form-group">
+            <label>Author</label>
+            <select name="author">
+              {authors.map((singleAuthor) => (
+                <option key={singleAuthor._id}>
+                  {singleAuthor.firstname + " " + singleAuthor.lastname}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-    );
+        <div className="flex justify-center">
+          {hideIndex === -1 && (
+            <button
+              type="button"
+              onClick={addSection}
+              className="flex items-center gap-1 text-xl bg-[#4299e1] text-white px-3 py-2 rounded"
+            >
+              <FiPlusCircle /> Add Section
+            </button>
+          )}
+        </div>
+        <div id="destination-div">
+          {sections.map((section, index) => (
+            <div key={section.id}>
+              <div className="bg-gray-200 p-10 m-10 rounded-md text-black fixing">
+                <div className="form-group">
+                  <label>Image</label>
+                  <input
+                    type="file"
+                    name={`image-${section.id}`}
+                    className="file-input file-input-bordered w-full max-w-xs"
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Header</label>
+                  <input type="text" name={`sectionHeader-${section.id}`} />
+                </div>
+                <div className="form-group">
+                  <label>Description</label>
+                  <JoditEditor
+                    name={`desc-${section.id}`}
+                    ref={editorRefs.current[index]}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex justify-center my-3">
+                {hideIndex === index && (
+                  <button
+                    type="button"
+                    onClick={addSection}
+                    className="flex items-center gap-1 text-xl bg-[#4299e1] text-white px-3 py-2 rounded"
+                  >
+                    <FiPlusCircle /> Add Section
+                  </button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="form-actions mx-10">
+          <button type="submit" className="btn-save">
+            Submit Blog
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddBlog;
